37f2ce8f412be067d10dc77edffed242446c7a16
sindy-leyva17 <lonilailan@gmail.com>
2024-05-05 17:31:35 -0500

    C# prep 1 ready

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace prep1
+{ 
+    class Program
     {
-        Console.WriteLine("Hello Prep1 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {lastname}, {name} {lastname}");
+        }
     }
 }
+
