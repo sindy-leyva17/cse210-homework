7e784d7892e1e7a4069492d2d1d7c490da7c144c
sindy-leyva17 <lonilailan@gmail.com>
2024-05-05 22:49:01 -0500

    prep3 first core

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace prep3
+{ 
+    class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the magic number ?");
+            string magicNumber = Console.ReadLine();
+
+            Console.WriteLine("What is your guess?");
+            string guess = Console.ReadLine();
+
+            int x = int.Parse(guess);
+            int y = int.Parse(magicNumber);
+
+            if (x > y)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("Higher")
+            }
+            else
+            {
+                Console.WriteLine("You get it")
+            }
+            
+        }
     }
 }
