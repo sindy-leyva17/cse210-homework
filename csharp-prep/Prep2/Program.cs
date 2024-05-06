fa9afd04ad75d7c2eacba5af4d9288a3089a6b33
sindy-leyva17 <lonilailan@gmail.com>
2024-05-05 20:55:40 -0500

    prep 2 first and second core

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,43 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace prep2
+{ 
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade percentage:");
+            string valueFromUser = Console.ReadLine();
+
+            int x = int.Parse(valueFromUser);
+
+            if (x >= 90)
+            {
+                Console.WriteLine("You got an A");
+            }
+            else if (x >= 80)
+            {
+                Console.WriteLine("You got a B");
+            }
+            else if (x >= 70)
+            {
+                Console.WriteLine("You got a C");
+            }
+            else if (x >= 60)
+            {
+                Console.WriteLine("You got a D");
+            }
+            else if (x < 60)
+            {
+                Console.WriteLine("You got a F");
+            }
+            if (x >= 90 || x >= 80 || x >= 70)
+            {
+                Console.WriteLine("Congratulations, you pass.");
+            }
+            else{
+                Console.WriteLine("Sorry , you do not pass.Good luck for the next one.");
+            }
+        }
     }
 }
