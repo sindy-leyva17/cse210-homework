2aa2364cc18e10df10de7000e5bb12ae8823de3d
sindy-leyva17 <lonilailan@gmail.com>
2024-05-06 02:20:53 -0500

    prep4 first second and third core

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Prep4
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep4 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            List<int> numbers = new List<int>();
+            int number;
+
+            do
+            {
+                Console.WriteLine("Enter a number:");
+                number = int.Parse(Console.ReadLine());
+                
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+            } while (number != 0);
+            
+            int sum = 0;
+            
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            Console.WriteLine($"The sum is: {sum}");
+            
+            float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+            
+            int max = int.MinValue; 
+
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine($"The largest number is: {max}");
+            
+            
+        }
     }
 }
